c4e10b6cac86a523243c17cb66e24f230a1fb5bc
meantg <43744275+meantg@users.noreply.github.com>
2021-12-21 11:06:03 +0700

    introduction reactjs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './screens/Home';
+import Calculator from './screens/Calculator';
+import LoginForm from './screens/LoginForm';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Login</Link>
+            </li>
+            <li>
+              <Link to="/calculator">Calculator</Link>
+            </li>
+            <li>
+              <Link to="/countdown">Count Down 2022</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Routes> and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/calculator">
+            <Calculator />
+          </Route>
+          <Route path="/countdown">
+            <Home />
+          </Route>
+          <Route path="/">
+            <LoginForm />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
